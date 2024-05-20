b025f5dc0aa5e66abd21eb386824d3232d2954f2
NataliaGus <163395093+NataliaGus@users.noreply.github.com>
2024-05-20 13:52:45 +0200

    20 mayo

---
+++ JS_NATALIA/EJERCICIO_1/script.js
@@ -0,0 +1,28 @@
+//1
+
+let nombre = "Natalia"
+console.log("Hola, " + nombre + "!");
+
+//2
+
+let edad = 23;
+ 
+console.log("Felicidades " + nombre + ", ya tienes " + edad + " !");
+
+let añoNacimiento = 2000;
+let añoActual = 2024;
+let añosVivimos = añoActual - añoNacimiento;
+
+console.log("Tienes " + añosVivimos + " años.");
+
+//3
+
+let nota1 = 8;
+let nota2 = 9;
+let nota3 = 5;
+
+let notaFinal = (nota1 + nota2 + nota3) /3;
+
+console.log("Tu calificación final es: " + notaFinal);
+
+
---
+++ JS_NATALIA/PROMPT/script.js
@@ -0,0 +1,26 @@
+//Operadores de asignación
+
+let x = 20;
+let y = 10;
+
+x += 6; //x = x + 6
+y-= 5; // x = y - 5
+
+
+console.log(x);
+console.log(y);
+
+
+//Comparación de valores
+
+let valor = 5;
+console.log ( valor == 5);
+console.log (valor === 5);
+
+//
+
+let nombre = prompt("Por favor, ingresa tu nombre:"); //lo guardas en la variable
+let apellido = prompt("Por favor, ingresa tu apellido:")
+
+
+console.log("Hola, " + nombre + " " + apellido + "!");
